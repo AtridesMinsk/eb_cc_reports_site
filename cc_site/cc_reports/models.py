c6044d043f24209d0242253e1c4430c38eaf113e
pbarabanau <atrides@gmail.com>
2022-02-07 16:07:28 +0300

    Ver 0.0.1: +Fixed: Clear not-used clas from models +Add: Add to requirements.txt connect_db package +Refactor: Refactor

--- cc_site/cc_reports/models.py
+++ cc_site/cc_reports/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.translation import gettext as _
 
 
 # Create your models here.
@@ -13,11 +12,3 @@ class Tasks(models.Model):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
-
-
-class AverageCallsReport(models.Model):
-    date = models.DateField(_("Дата"), auto_now=True)
-    calls_count = models.IntegerField(_("Количество звонков"))
-    average_call_time = models.TimeField(_("Среднее время звонка"), auto_now=True)
-    average_ivr_time = models.TimeField(_("Среднее время IVR"), auto_now=True)
-    average_ringing_time = models.TimeField(_("Среднее время ответа оператора"), auto_now=True)
