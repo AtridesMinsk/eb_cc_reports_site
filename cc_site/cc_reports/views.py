66623a8cc954e4a256fc00161517dd2bc818af2d
pbarabanau <atrides@gmail.com>
2022-02-14 08:34:43 +0300

    Ver 0.0.4: Fixed: Refactor

--- cc_site/cc_reports/views.py
+++ cc_site/cc_reports/views.py
@@ -24,14 +24,16 @@ def average_call_rep(request):
 
 
 def get_data_drop_call(call_id):
-    sql_request = (f'SELECT callcent_ag_dropped_calls.ag_num, '
-                   f'callcent_ag_dropped_calls.time_start AT TIME ZONE \'UTC+3\', '
-                   f'callcent_ag_dropped_calls.time_end AT TIME ZONE \'UTC+3\', '
-                   f'DATE_TRUNC(\'second\', callcent_ag_dropped_calls.ts_polling + interval \'500 millisecond\'), '
-                   f'callcent_ag_dropped_calls.reason_noanswerdesc, callcent_ag_dropped_calls.q_call_history_id, '
-                   f'callcent_queuecalls.from_userpart FROM callcent_ag_dropped_calls LEFT JOIN callcent_queuecalls '
-                   f'ON callcent_queuecalls.call_history_id = q_call_history_id WHERE q_call_history_id = '
-                   f'\'{call_id}\' ORDER BY idcallcent_ag_dropped_calls ASC')
+    sql_request = (f"""
+            SELECT callcent_ag_dropped_calls.ag_num,
+            callcent_ag_dropped_calls.time_start AT TIME ZONE 'UTC+3',
+            callcent_ag_dropped_calls.time_end AT TIME ZONE 'UTC+3',
+            DATE_TRUNC('second', callcent_ag_dropped_calls.ts_polling + interval '500 millisecond'),
+            callcent_ag_dropped_calls.reason_noanswerdesc, callcent_ag_dropped_calls.q_call_history_id,
+            callcent_queuecalls.from_userpart FROM callcent_ag_dropped_calls LEFT JOIN callcent_queuecalls
+            ON callcent_queuecalls.call_history_id = q_call_history_id WHERE q_call_history_id =
+            '{call_id}' ORDER BY idcallcent_ag_dropped_calls ASC
+    """)
 
     try:
         connection = psycopg2.connect(database=database,
@@ -65,15 +67,15 @@ def drop_call(request):
 
 
 def get_data_all_drop_call():
-    sql_request = (
-        f'SELECT callcent_ag_dropped_calls.ag_num, callcent_ag_dropped_calls.time_start AT TIME ZONE \'UTC+3\', '
-        f'callcent_ag_dropped_calls.time_end AT TIME ZONE \'UTC+3\', '
-        f'DATE_TRUNC(\'second\', callcent_ag_dropped_calls.ts_polling + interval '
-        f'\'500 millisecond\'), callcent_ag_dropped_calls.reason_noanswerdesc, '
-        f'callcent_ag_dropped_calls.q_call_history_id FROM callcent_ag_dropped_calls WHERE reason_noanswerdesc = '
-        f'\'Poll expired\' AND time_start AT TIME ZONE \'UTC+3\' > \'2021-08-01\' ORDER BY '
-        f'idcallcent_ag_dropped_calls DESC '
-    )
+    sql_request = (f"""
+            SELECT callcent_ag_dropped_calls.ag_num, callcent_ag_dropped_calls.time_start AT TIME ZONE 'UTC+3',
+            callcent_ag_dropped_calls.time_end AT TIME ZONE 'UTC+3',
+            DATE_TRUNC('second', callcent_ag_dropped_calls.ts_polling + interval
+            '500 millisecond'), callcent_ag_dropped_calls.reason_noanswerdesc,
+            callcent_ag_dropped_calls.q_call_history_id FROM callcent_ag_dropped_calls WHERE reason_noanswerdesc =
+            'Poll expired' AND time_start AT TIME ZONE 'UTC+3' > '2021-08-01' ORDER BY
+            idcallcent_ag_dropped_calls DESC
+    """)
 
     try:
         connection = psycopg2.connect(database=database,
@@ -109,43 +111,45 @@ def all_drop_call(request):
 
 
 def get_data_calls_by_operator():
-    sql_request = (
-        f'WITH '
-        f'Canceled_calls AS ( '
-        f'SELECT count (*) AS Call_count, ag_num '
-        f'FROM callcent_ag_dropped_calls '
-        f'WHERE time_start AT TIME ZONE \'UTC+3\' > \'2021-08-01\' AND reason_noanswerdesc = \'Cancelled\' AND ag_num '
-        f'!= \'1000\' AND ag_num != \'1001\' AND ag_num != \'9999\' '
-        f'GROUP BY ag_num '
-        f'ORDER BY ag_num '
-        f'), '
-        f'Call_in AS ( '
-        f'SELECT to_dn AS Operator_ID_in, count (*) AS Calls_by_Operator_in '
-        f'FROM callcent_queuecalls '
-        f'WHERE ts_servicing != \'00:00:00\' AND time_start AT TIME ZONE \'UTC+3\' > \'2021-08-01\' AND to_dn != '
-        f'\'1000\' AND to_dn != \'1001\' AND to_dn != \'9999\' '
-        f'GROUP BY to_dn '
-        f'ORDER BY to_dn ASC '
-        f'), '
-        f'Call_out AS (SELECT count (*) AS Calls_by_Operator_out, si.dn AS Operator_ID_out '
-        f'FROM ((((((cl_segments s '
-        f'JOIN cl_participants sp ON ((sp.id = s.src_part_id))) '
-        f'JOIN cl_participants dp ON ((dp.id = s.dst_part_id))) '
-        f'JOIN cl_party_info si ON ((si.id = sp.info_id))) '
-        f'JOIN cl_party_info di ON ((di.id = dp.info_id))) '
-        f'LEFT JOIN cl_participants ap ON ((ap.id = s.action_party_id))) '
-        f'LEFT JOIN cl_party_info ai ON ((ai.id = ap.info_id))) '
-        f'WHERE s.start_time AT TIME ZONE \'UTC-3\' > \'2021-08-01\' '
-        f'AND s.action_id = 1 AND si.dn_type = 0 AND di.dn_type = 13 AND seq_order = 1 '
-        f'AND si.dn != \'1000\' AND si.dn != \'1001\' '
-        f'GROUP BY si.dn '
-        f'ORDER BY si.dn ASC) '
-        f'SELECT Call_in.Operator_ID_in, Call_in.Calls_by_Operator_in, Call_out.Calls_by_Operator_out, '
-        f'Canceled_calls.Call_count '
-        f'FROM Call_in '
-        f'INNER JOIN Call_out ON Call_in.Operator_ID_in = Call_out.Operator_ID_out '
-        f'INNER JOIN Canceled_calls ON Call_in.Operator_ID_in = Canceled_calls.ag_num '
-    )
+    sql_request = (f"""
+            WITH
+            Canceled_calls AS (
+                SELECT count (*) AS Call_count, ag_num
+                FROM callcent_ag_dropped_calls
+                WHERE time_start AT TIME ZONE 'UTC+3' > '2021-08-01' AND reason_noanswerdesc = 'Cancelled' AND ag_num
+                != '1000' AND ag_num != '1001' AND ag_num != '9999'
+                GROUP BY ag_num
+                ORDER BY ag_num
+            ),
+            Call_in AS (
+                SELECT to_dn AS Operator_ID_in, count (*) AS Calls_by_Operator_in
+                FROM callcent_queuecalls
+                WHERE ts_servicing != '00:00:00' AND time_start AT TIME ZONE 'UTC+3' > '2021-08-01' AND to_dn !=
+                '1000' AND to_dn != '1001' AND to_dn != '9999'
+                GROUP BY to_dn
+                ORDER BY to_dn ASC
+            ),
+            Call_out AS (
+                SELECT count (*) AS Calls_by_Operator_out, si.dn AS Operator_ID_out
+                FROM ((((((cl_segments s
+                JOIN cl_participants sp ON ((sp.id = s.src_part_id)))
+                JOIN cl_participants dp ON ((dp.id = s.dst_part_id)))
+                JOIN cl_party_info si ON ((si.id = sp.info_id)))
+                JOIN cl_party_info di ON ((di.id = dp.info_id)))
+                LEFT JOIN cl_participants ap ON ((ap.id = s.action_party_id)))
+                LEFT JOIN cl_party_info ai ON ((ai.id = ap.info_id)))
+                WHERE s.start_time AT TIME ZONE 'UTC-3' > '2021-08-01'
+                AND s.action_id = 1 AND si.dn_type = 0 AND di.dn_type = 13 AND seq_order = 1
+                AND si.dn != '1000' AND si.dn != '1001'
+                GROUP BY si.dn
+                ORDER BY si.dn ASC              
+            )
+            SELECT Call_in.Operator_ID_in, Call_in.Calls_by_Operator_in, 
+            Call_out.Calls_by_Operator_out, Canceled_calls.Call_count
+            FROM Call_in
+            INNER JOIN Call_out ON Call_in.Operator_ID_in = Call_out.Operator_ID_out
+            INNER JOIN Canceled_calls ON Call_in.Operator_ID_in = Canceled_calls.ag_num
+            """)
 
     try:
         connection = psycopg2.connect(database=database,
