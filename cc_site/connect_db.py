74047f93ce40fb2f425159deb29b4e7d941f10cb
Paul Atrides <90185826+AtridesMinsk@users.noreply.github.com>
2022-03-14 13:18:39 +0300

    Create connect_db.py
    sample of connect_db.py

---
+++ cc_site/connect_db.py
@@ -0,0 +1,15 @@
+prod_password = "password"
+prod_host = "ip addres"
+
+test_password = "password"
+test_host_prod = "ip addres"
+
+user = "phonesystem"
+database = "db name"
+port = "5432"
+
+core_host = "ip addres"
+core_port = "3306"
+core_db = "db name"
+core_user = "user name"
+core_password = "password"
